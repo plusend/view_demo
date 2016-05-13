251ccb22391c9653dbb98b735967ad6e539cd1e7
jian.zhang <jian.zhang@yulore.com>
2016-05-13 16:29:43 +0800

    继承 ViewGroup 派生特殊的 Layout

---
+++ app/src/main/java/com/plusend/viewdemo/activity/HorizontalScrollViewExActivity.java
@@ -0,0 +1,15 @@
+package com.plusend.viewdemo.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.plusend.viewdemo.R;
+
+public class HorizontalScrollViewExActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horizontal_scroll_view_ex);
+    }
+}
